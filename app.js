34a79edd2910161a7c25ef03d4884e44c20a66c5
y-kurami <yosuke.kurami@gmail.com>
2017-07-17 20:43:39 +0900

    Updates

---
+++ app.js
@@ -0,0 +1,20 @@
+const { decode } = require("../../");
+
+const elm = document.getElementById("drop");
+
+elm.addEventListener("dragenter", e => e.preventDefault());
+elm.addEventListener("dragover", e => e.preventDefault());
+elm.addEventListener("drop", (e) => {
+  e.preventDefault();
+  const file = e.dataTransfer.files[0];
+  const reader = new FileReader();
+  reader.addEventListener("load", (e) => {
+    const arrayBuffer = e.target.result;
+    const { width, height, ifdEntries } = decode(arrayBuffer);
+    const IFDs = JSON.stringify({ width, height, ifdEntries }, null, 2);
+    elm.innerHTML = `
+      <pre>${IFDs}</pre>
+    `;
+  });
+  reader.readAsArrayBuffer(file);
+});
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  entry: {
+    app: __dirname + "/app.js"
+  },
+  output: {
+    path: __dirname,
+    filename: "bundle.js",
+  }
+};
